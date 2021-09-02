a67fcb0f06c379e349cd8996ca42970da9dd928f
hbybyyang <2451759073@qq.com>
2021-09-02 12:14:13 +0800

    .

--- test/data.ts
+++ test/data.ts
@@ -1,4 +1,4 @@
 export interface 请求数据 {
-  a: number
-  b: number
+    a: number
+    b: number
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -5,18 +5,18 @@ import main from '../dist/'
 var 验证器 = main(path.resolve(__dirname, './data.ts'))
 
 describe('基本测试', function () {
-  // it('构建缓存', async function () {
-  //     this.timeout(9999999)
-  //     验证器.构建缓存()
-  // })
-  it('验证', async function () {
-    this.timeout(9999999)
-    var 结果 = await 验证器.验证('请求数据', { a: 1, b: 1 })
-    tools.断言相等(结果.valid, true)
-  })
-  it('验证', async function () {
-    this.timeout(9999999)
-    var 结果 = await 验证器.验证('请求数据', { a: '1', b: 1 })
-    tools.断言相等(结果.valid, false)
-  })
+    // it('构建缓存', async function () {
+    //     this.timeout(9999999)
+    //     验证器.构建缓存()
+    // })
+    it('验证', async function () {
+        this.timeout(9999999)
+        var 结果 = await 验证器.验证('请求数据', { a: 1, b: 1 })
+        tools.断言相等(结果.valid, true)
+    })
+    it('验证', async function () {
+        this.timeout(9999999)
+        var 结果 = await 验证器.验证('请求数据', { a: '1', b: 1 })
+        tools.断言相等(结果.valid, false)
+    })
 })
